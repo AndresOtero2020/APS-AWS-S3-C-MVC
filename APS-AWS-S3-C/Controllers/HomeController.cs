21a4b1dcb1b76b3d84c51a8fd628c1e52d59d2b3
Andres Otero <andresjuarez915@gmail.com>
2026-05-14 23:16:49 -0600

    Se ejecutan los comando para poder insertar en el AWS

--- APS-AWS-S3-C/Controllers/HomeController.cs
+++ APS-AWS-S3-C/Controllers/HomeController.cs
@@ -1,8 +1,17 @@
-using APS_AWS_S3_C.Data;
-using APS_AWS_S3_C.Models;
+using APS_AWS_S3_C.Data; //
+using APS_AWS_S3_C.Models; //
 using Microsoft.AspNetCore.Identity.Data;
-using Microsoft.AspNetCore.Mvc;
-using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;  //
+using System.Diagnostics; //
+//Hacemos la referencias a las librerias 
+using Amazon;
+using Amazon.S3;
+using Amazon.S3.Model;
+using Amazon.S3.Transfer;
+// para poder usar el IFormFile
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Components.Forms;
+
 
 namespace APS_AWS_S3_C.Controllers
 {
@@ -23,20 +32,72 @@ namespace APS_AWS_S3_C.Controllers
         public IActionResult Index()
         {
             // Colocamos el listado de los archivos
-            List<S3FileDetais> file=new List<S3FileDetais>();
+            List<S3FileDetais> files=new List<S3FileDetais>();
             // Lo que retornamos en la lista 
-            file = _db.s3FileDetais.ToList();
+            files = _db.s3FileDetais.ToList();
 
             // Lo que mostraremos en la lista.
-            return View(file);
+            return View(files);
         }
 
-        // Creamos la ruta 
+        // Codigo para el s3 ASW 
         [HttpPost]
-        public async Task<AcctionsResult> UploadFileToS3(IformFile file)
+        public async Task<IActionResult> UploadfileToS3(IFormFile file)
         {
+			// 1. Validación de seguridad: Si el archivo es nulo, devolvemos un error
+	    	//	if (file == null || file.Length == 0)
+		    //	{
+		    //		return BadRequest("No se ha seleccionado ningún archivo.");
+		    //	}
 
-        }
+			// Colocamos la claves que generamos en el amazon S3
+			// Usamos el cliente de Amazon
+			using ( var amazonS3client= new AmazonS3Client(
+                // Se coloca la claves de S3
+                "AKIAXIRJEVYXVJRCZ74Y",
+                "IMRwyRzo4b0cVudWjW6X+eyx12KtQkiyQFvAFt87",
+                /// Colocamos la region del AWS 
+				Amazon.RegionEndpoint.USEast2
+				))
+			{
+                using (var memorystream =  new MemoryStream())
+				{
+                    file.CopyTo(memorystream);
+                    var request = new TransferUtilityUploadRequest
+					{
+                        // Hacemos el llamado al buken
+                        InputStream = memorystream,
+                        Key = file.FileName,
+                        // Nombre del buken 
+                        BucketName = "s3-prenube26",
+                        ContentType = file.ContentType,
+                    };
+                    var TranferUtility = new TransferUtility(amazonS3client);
+                    await TranferUtility.UploadAsync(request);
+
+				}
+
+			}
+            // hacemos el llamado al model de s3
+            S3FileDetais fileDetais =new S3FileDetais();
+            fileDetais.FileName=file.FileName;
+            /// almacenamos la fecha 
+            fileDetais.FileDate = DateTime.Today;
+
+
+            _db.s3FileDetais.Add(fileDetais);
+            //Los guardamos 
+            _db.SaveChanges();
+
+            //Mostramos el mensaje
+            ViewBag.Success = "Archivo subido con éxito on S3 Bukent";
+
+            //hacemos el return
+            return RedirectToAction(nameof(Index));
+
+			//return "Archivo subido";
+			//return Ok(new { mensaje = "Archivo subido con éxito", nombre = file.FileName });
+		}
 
         public IActionResult Privacy()
         {
